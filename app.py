3018df9239fd7d8402804173f85d2f6c8bb1bb47
adty2024 <avisaxena3019@gmail.com>
2023-06-06 08:00:45 +0530

    language

--- app.py
+++ app.py
@@ -5,18 +5,45 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app, origins=['http://127.0.0.1:5173', 'https://chainlinkapi-production.up.railway.app', 'https://chainlink-audio.onrender.com'])
 
-@app.route("/<int:param>", methods=["GET"])
-def predict(param):
+@app.route("/<string:char_param>/<int:param>", methods=["GET"])
+def predict(char_param, param):
     try:
-        if param == 8:
-            return send_file("output pdf 8.mp3", mimetype="audio/mpeg")
-        elif param == 9:
-            return send_file("output pdf 9.mp3", mimetype="audio/mpeg")
-        elif param == 11:
-            return send_file("output pdf 11.mp3", mimetype="audio/mpeg")
+        if char_param == 'e':
+            if param == 1:
+                return send_file("e1.mp3", mimetype="audio/mpeg")
+            elif param == 2:
+                return send_file("e2.mp3", mimetype="audio/mpeg")
+            elif param == 3:
+                return send_file("e3.mp3", mimetype="audio/mpeg")
+            elif param == 4:
+                return send_file("e4.mp3", mimetype="audio/mpeg")
+            elif param == 5:
+                return send_file("e5.mp3", mimetype="audio/mpeg")
+        elif char_param == 'h':
+            if param == 1:
+                return send_file("h1.mp3", mimetype="audio/mpeg")
+            elif param == 2:
+                return send_file("h2.mp3", mimetype="audio/mpeg")
+            elif param == 3:
+                return send_file("h3.mp3", mimetype="audio/mpeg")
+            elif param == 4:
+                return send_file("h4.mp3", mimetype="audio/mpeg")
+            elif param == 5:
+                return send_file("h5.mp3", mimetype="audio/mpeg")
+        else:
+            if param == 1:
+                return send_file("b1.mp3", mimetype="audio/mpeg")
+            elif param == 2:
+                return send_file("b2.mp3", mimetype="audio/mpeg")
+            elif param == 3:
+                return send_file("b3.mp3", mimetype="audio/mpeg")
+            elif param == 4:
+                return send_file("b4.mp3", mimetype="audio/mpeg")
+            elif param == 5:
+                return send_file("b5.mp3", mimetype="audio/mpeg")
     except Exception as e:
         return jsonify({"error": str(e)})
-    
+
 
 @app.route('/favicon.ico')
 def favicon():
